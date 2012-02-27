3d24d97ccf8898f95ad9baf64a7bb4326d0d6d99
ohdae <bindshell@live.com>
2012-02-27 16:22:51 -0500

    Added code to pull and save external IP

--- Intersect2-Beta.py
+++ Intersect2-Beta.py
@@ -291,6 +291,7 @@ def NetworkMap():
    # Combine ARP then portscan. Send IPs to list and iterate through for the scan
    # Add service identification via socket for all open ports
    # Add traceroute after finding live hosts. Send all results to graph report.
+   # import scapy.all
    
     print("[+] Searching for live hosts...")
     os.mkdir(Temp_Dir+"/hosts")
@@ -310,6 +311,15 @@ def NetworkMap():
         file.write("\n\n[+] Live Host\nIP: "+ip_address + " MAC"+ mac_address + "\n")
     file.write("\n")
     file.close
+
+
+    externalIP = ip = urllib2.urlopen("http://whatismyip.org").read()
+    file = open("external.txt", "a")
+    file.write("External IP Address: " + externalIP +"\n\n")
+    file.close
+
+
+
 
 # --------------- ARP scan then SYN scan each live IP ---------------------------------
 #print("[-] Searching for live hosts...")
