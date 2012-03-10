58860e82e3d818df872b0b1654d78a58b19db574
ohdae <bindshell@live.com>
2012-03-09 22:48:32 -0500

    Fixed accidental commits

--- Create/CreateUtility.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-# Creation Utility
-
--- Create/Template.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# Intersect 2.5
-# Template for Create.py
-# Includes any nessicary libraries, usage, etc
-# Create.py will let the user select which functions they wish to include (stored as separate files)
-# then make a new copy of template.py, insert the selected functions and save
-
